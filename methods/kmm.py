68b146ff86d9413132ea4ef83edea94e4b7461e2
jmliu <jmliu206@gmail.com>
2023-03-09 19:02:48 +0900

    [update] add kmm method (cpu)

---
+++ methods/kmm.py
@@ -0,0 +1,50 @@
+import math
+import sklearn.metrics.pairwise as sk
+from cvxopt import matrix, solvers
+import numpy as np
+
+
+# compute weights using Kernel Mean Matching.
+# returns a list of weights for training data.
+def kmm(x_train, x_test, sigma):
+    x_train = x_train.astype(np.double)
+    x_test = x_test.astype(np.double)
+    n_tr = len(x_train)
+    n_te = len(x_test)
+
+    # calculate Kernel
+    K = sk.rbf_kernel(x_train, x_train, sigma)
+    # regularization
+    K = K + 0.00001 * np.identity(n_tr)
+
+    # calculate kappa
+    kappa_r = sk.rbf_kernel(x_train, x_test, sigma)
+    ones = np.ones(shape=(n_te, 1))
+    kappa = np.dot(kappa_r, ones)
+    kappa = -(float(n_tr) / float(n_te)) * kappa
+
+    # calculate eps
+    eps = (math.sqrt(n_tr) - 1) / math.sqrt(n_tr)
+    # constraints
+    A0 = np.ones(shape=(1, n_tr))
+    A1 = -np.ones(shape=(1, n_tr))
+    A = np.vstack([A0, A1, -np.eye(n_tr), np.eye(n_tr)])
+    b = np.array([[n_tr * (eps + 1), n_tr * (eps - 1)]])
+    b = np.vstack([b.T, -np.zeros(shape=(n_tr, 1)), np.ones(shape=(n_tr, 1)) * 50])
+
+    P = matrix(K, tc='d')
+    q = matrix(kappa, tc='d')
+    G = matrix(A, tc='d')
+    h = matrix(b, tc='d')
+    solvers.options['show_progress'] = False
+    beta = solvers.qp(P, q, G, h)
+    return [i for i in beta['x']]
+
+
+# compute the kernel width
+def get_kernel_width(data):
+    dist = []
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            dist.append(np.sqrt(np.sum((np.array(data[i]) - np.array(data[j])) ** 2)))
+    return np.quantile(np.array(dist), 0.01)
